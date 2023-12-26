09d19d55f806e9a1384f2e389a2e7bbba7062831
DuHyeon <korjack@duhyeon.kim>
2023-12-26 22:05:22 +0900

    feat: 배럴 폭발 반경 적용, 총 발사 소리 추가, 총구 화염 이미지추가
    - 배럴
        - 배럴 레이어 추가
        - 배럴 폭발시, 주변 배럴들에게 영향을 줌 (폭발하진 않음)
    - 총 발사 소리 추가
        - 총 발사 시, 소리 재생
        - 배럴 및 벽과 가까이 혹은 관통 후 발사하면, 소리가 나지 않는 것 해결
    - 총구 화염
        - 이미지만 추가

--- Assets/02.Scripts/BarrelCtrl.cs
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -11,6 +11,14 @@ public class BarrelCtrl : MonoBehaviour
     
     // 무작위 텍스쳐
     public Texture[] textures;
+    
+    //폭발 반경
+    public float radius = 10.0f;
+    
+    // 반경내로 검출되는 Collider 값을 미리 선언
+    // 메모리 Garbage를 발생을 줄이기 위함
+    // 미리 최대 갯수를 알 수 있다면 이렇게 하는 것이 좋음
+    private Collider[] colls = new Collider[10];
 
     private Transform tr;
     private Rigidbody rb;
@@ -53,13 +61,42 @@ public class BarrelCtrl : MonoBehaviour
         // 폭발 오브젝트 삭제
         Destroy(exp, 0.5f);
 
-        // 무게를 가볍게 변경
-        rb.mass = 1.0f;
-        
-        // 위쪽 방향으로 힘 추가
-        rb.AddForce(Vector3.up * 1500.0f);
+        // // 무게를 가볍게 변경
+        // rb.mass = 1.0f;
+        //
+        // // 위쪽 방향으로 힘 추가
+        // rb.AddForce(Vector3.up * 1500.0f);
+
+        // 폭발 전달
+        IndirectDamage(tr.position);
         
         // 3초 후 드럼통 제거
         Destroy(gameObject, 3.0f);
+    }
+
+    void IndirectDamage(Vector3 pos)
+    {
+        // 반경내의 드럼통 수집
+        // 아래와 같이 진행하면, 메모리 Garbage가 발생한다.
+        // 검출될 갯수가 명확하지 않다면 사용해야하기는 한다.
+        // Collider[] colls = Physics.OverlapSphere(pos, radius, 1 << 3);
+
+        // 메모리 Garbage가 일어나지 않고 사용하기 위한 방법
+        Physics.OverlapSphereNonAlloc(pos, radius, colls, 1 << 3);
+
+        foreach (var coll in colls)
+        {
+            // 드럼통 Rigidbody 추출
+            rb = coll.GetComponent<Rigidbody>();
+            
+            // 무게를 가볍게 한다 
+            rb.mass = 1.0f;
+            
+            // freezeRotation 제한값 해제
+            rb.constraints = RigidbodyConstraints.None;
+            
+            // 폭발력 전달
+            rb.AddExplosionForce(1500.0f, pos, radius, 1200.0f);
+        }
     }
 }
--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 반드시 필요한 컴포넌트를 명시해 해당 컴포넌트가 삭제되는 것을 방지하는 속성
+[RequireComponent(typeof(AudioSource))]
 public class FireCtrl : MonoBehaviour
 {
     // 총알
@@ -10,6 +12,26 @@ public class FireCtrl : MonoBehaviour
 
     // 총알 발사 위치
     public Transform firePos;
+    
+    // 총소리에 사용될 오디오
+    public AudioClip fireSfx;
+    
+    // AudioSource 컴포넌트를 저장할 변수
+    private new AudioSource audio;
+    
+    // Muzzle Flash의 MeshRenderer 컴포넌트
+    private MeshRenderer muzzleFlash;
+
+    private void Start()
+    {
+        audio = GetComponent<AudioSource>();
+        
+        // FirePos 하위에 있는 MuzzleFlash 추출
+        muzzleFlash = GetComponentInChildren<MeshRenderer>();
+        
+        // 처음 시작시 비활성화
+        muzzleFlash.enabled = false;
+    }
 
     private void Update()
     {
@@ -21,6 +43,10 @@ public class FireCtrl : MonoBehaviour
 
     void Fire()
     {
+        // 총알 생성
         Instantiate(bullet, firePos.position, firePos.rotation);
+        
+        // 총소리 발생
+        audio.PlayOneShot(fireSfx, 1.0f);
     }
 }
