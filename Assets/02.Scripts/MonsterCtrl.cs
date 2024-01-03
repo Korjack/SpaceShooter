2af8de265680d75f62eb38583b3b961c1bfb25ee
DuHyeon <korjack@duhyeon.kim>
2024-01-03 21:56:57 +0900

    feat: 네비게이션 메쉬 생성, 몬스터 상태 체크 및 변경, 몬스터 추격 및 공격 에니매이터 추가
    - 네비게이션
        - 유니티 AI 네비게이션 사용
        - Floor에 NavMeshSurface 추가
        - 몬스터에 NavMeshAgent 추가
        - 몬스터가 플레이어를 쫒던 도중 2유닛 거리에서 멈춤
    - 몬스터
        - 몬스터 각 상태 별로 행동
        - 상태 변경 및 몬스터 행동은 코루틴에서 0.3초 기준으로 Update
        - 상태는 DIE, IDE, TRACE, ATTACK 있음
        - 상태에 따라 애니메이터 실행(IDE, TRACE, ATTACK만)
        - 공격 애니메이터 추가
        - 설정된 사정거리에 들어올 경우 몬스터가 추격함
        - 기즈모로 상황에 맞는 사거리 디버깅 추가

---
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -0,0 +1,139 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterCtrl : MonoBehaviour
+{
+    private Transform monsterTr;
+    private Transform playerTr;
+    private NavMeshAgent _agent;
+    
+    // 몬스터 상태 정보
+    public enum State 
+    {
+        IDLE,
+        TRACE,
+        ATTACK,
+        DIE
+    }
+
+    // 몬스터 현재 상태
+    public State state = State.IDLE;
+    // 추적 사정거리
+    public float traceDist = 10.0f;
+    // 공격 사정거리
+    public float attackDis = 2.0f;
+    // 몬스터 사망 여부
+    public bool isDie = false;
+    
+    //애니메이터
+    private Animator anim;
+    private static readonly int IsTrace = Animator.StringToHash("IsTrace");
+    private static readonly int IsAttack = Animator.StringToHash("IsAttack");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 각 컴포넌트 로드
+        monsterTr = GetComponent<Transform>();
+        playerTr = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        _agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        // 몬스터 추적 바로 시작
+        // _agent.destination = playerTr.position;
+        
+        // 몬스터 상태 업로드 코루틴 호출
+        StartCoroutine(CheckMonsterState());
+        
+        // 몬스터 상태에 따른 행동
+        StartCoroutine(MonsterAction());
+    }
+
+    IEnumerator CheckMonsterState()
+    {
+        while (!isDie)
+        {
+            // 0.3초 대기
+            yield return new WaitForSeconds(0.3f);
+            
+            // 몬스터와 주인공 사이의 거리 측정
+            float distance = Vector3.Distance(playerTr.position, monsterTr.position);
+            
+            // 공격 사정거리 범위로 들어왔는지 확인
+            if (distance <= attackDis)
+            {
+                state = State.ATTACK;
+            }
+            // 추적 사정거리 범위 안에 들어왔다면?
+            else if (distance <= traceDist)
+            {
+                state = State.TRACE;
+            }
+            else
+            {
+                state = State.IDLE;
+            }
+        }
+    }
+
+    IEnumerator MonsterAction()
+    {
+        while (!isDie)
+        {
+            switch (state)
+            {
+                // IDE 상태
+                case State.IDLE:
+                    // 추격 중단
+                    _agent.isStopped = true;
+                    // 에니매이터의 IsTrace 변수를 false
+                    anim.SetBool(IsTrace, false);
+                    break;
+                // 추적 상태
+                case State.TRACE:
+                    // 추격 대상으로 이동
+                    _agent.SetDestination(playerTr.position);
+                    _agent.isStopped = false;
+                    
+                    // 에니매이터의 IsTrace 변수를 true
+                    anim.SetBool(IsTrace, true);
+                    
+                    // 에니메이터의 IsAttack 변수를 false
+                    anim.SetBool(IsAttack, false);
+                    break;
+                // 공격 상태
+                case State.ATTACK:
+                    // 에니메이터의 IsAttack 변수를 true
+                    anim.SetBool(IsAttack, true);
+                    break;
+                // 사망
+                case State.DIE:
+                    break;
+            }
+
+            yield return new WaitForSeconds(0.3f);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // 추적 사정거리 표기
+        if (state == State.TRACE)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, traceDist);
+        }
+        
+        // 공격 사정거리 표기
+        if (state == State.ATTACK)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(transform.position, attackDis);
+            
+        }
+    }
+}
