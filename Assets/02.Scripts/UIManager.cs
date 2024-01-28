73437fa7add0a58c6525cfc6930bf0bb396e5789
DuHyeon <korjack@duhyeon.kim>
2024-01-28 15:26:03 +0900

    feat: 메뉴씬 추가, 메뉴 기능
    - 메뉴 씬 추가
    - 텍스트, 버튼, 폰트 추가
    - 백그라운드 이미지 추가

---
+++ Assets/02.Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    // 버튼 변수 3개
+    public Button startButton;
+    public Button optionButton;
+    public Button shopButton;
+
+    private UnityAction _action;
+
+    private void Start()
+    {
+        // UnityAction을 사용한 이벤트 연결 방식
+        _action = () => OnButtonClick(startButton.name);
+        startButton.onClick.AddListener(_action);
+        
+        // 무명 메서드를 활용한 이벤트 연결 방식
+        optionButton.onClick.AddListener(delegate { OnButtonClick(optionButton.name); });
+        
+        // 람다식을 활용한 이벤트 연결 방식
+        shopButton.onClick.AddListener(()=> OnButtonClick(shopButton.name));
+    }
+
+    public void OnButtonClick(string msg)
+    {
+        Debug.Log($"Button Clicked!  {msg}");
+    }
+}
