5cea189f23c3537731a37b70f6ff604d446cc2a9
DuHyeon <korjack@duhyeon.kim>
2024-03-02 16:48:31 +0900

    feat: 다양한 InputAction 추가 및 테스트

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -10,10 +10,49 @@ public class PlayerCtrl : MonoBehaviour
     private new Transform _transform;
     private Vector3 moveDir;
 
+    private PlayerInput _playerInput;
+    private InputActionMap mainActionMap;
+    private InputAction moveAction;
+    private InputAction attackAction;
+
     private void Start()
     {
         _animator = GetComponent<Animator>();
         _transform = GetComponent<Transform>();
+        _playerInput = GetComponent<PlayerInput>();
+
+        #region INPUT_SYSTEM
+
+        // ActionMap 추출
+        mainActionMap = _playerInput.actions.FindActionMap("PlayerActions");
+        
+        // Move, Attack 액션 추출
+        moveAction = mainActionMap.FindAction("Move");
+        attackAction = mainActionMap.FindAction("Attack");
+        
+        // Move 액션의 performed 이벤트 연결
+        moveAction.performed += ctx =>
+        {
+            Vector2 direction = ctx.ReadValue<Vector2>();
+            moveDir = new Vector3(direction.x, 0, direction.y);
+            _animator.SetFloat("Movement", direction.magnitude);
+        };
+        
+        // Move 액션의 canceled 이벤트 연결
+        moveAction.canceled += ctx =>
+        {
+            moveDir = Vector3.zero;
+            _animator.SetFloat("Movement", 0.0f);
+        };
+        
+        // Attack 액션의 performed 이벤트 연결
+        attackAction.performed += ctx =>
+        {
+            Debug.Log("Attack by c# Event");
+            _animator.SetTrigger("Attack");
+        };
+
+        #endregion
     }
 
     private void Update()
@@ -26,6 +65,8 @@ public class PlayerCtrl : MonoBehaviour
             _transform.Translate(Vector3.forward * (Time.deltaTime * 4.0f));
         }
     }
+
+    #region SEND_MESSAGE
 
     void OnMove(InputValue value)
     {
@@ -41,4 +82,34 @@ public class PlayerCtrl : MonoBehaviour
         Debug.Log("Attack");
         _animator.SetTrigger("Attack");
     }
+
+    #endregion
+
+    #region UNITY_EVENTS
+
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        Vector2 direction = ctx.ReadValue<Vector2>();
+        
+        // 2차원 좌표를 3차원 좌표로 변환
+        moveDir = new Vector3(direction.x, 0, direction.y);
+        
+        // Warrior_Run 애니매이션 실행
+        _animator.SetFloat("Movement", direction.magnitude);
+    }
+    
+    public void OnAttack(InputAction.CallbackContext ctx)
+    {
+        Debug.Log($"ctx.phase = {ctx.phase}");
+
+        if (ctx.performed)
+        {
+            Debug.Log("Attack");
+            _animator.SetTrigger("Attack");
+        }
+    }
+  
+
+    #endregion
+    
 }
